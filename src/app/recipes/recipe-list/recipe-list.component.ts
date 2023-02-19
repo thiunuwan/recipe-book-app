9449df468b41c8942b7eeb5130fe1188037bf736
thiunuwan <ravinduthiunuwan@gmail.com>
2023-02-19 09:04:13 +0530

    add recipe model

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {RecipeModel} from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  recipes:RecipeModel[]=[
+    new RecipeModel("test-recipe 1","test recipe one desc","https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=375,341")
+
+  ];
+
 
 }
---
+++ src/app/recipes/recipe.model.ts
@@ -0,0 +1,12 @@
+
+export class RecipeModel{
+  public name:string;
+  public description:string;
+  public imagePath:string;
+
+  constructor(name:string,desc:string,imgPath:string) {
+  this.name=name;
+  this.description=desc;
+  this.imagePath=imgPath;
+  }
+}
